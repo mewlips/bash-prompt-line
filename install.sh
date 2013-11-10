e93a37a34b376d73aa97acd387f79d80133841bd
mewlips <mewlips@gmail.com>
2013-11-10 18:32:47 +0900

    fix install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-ln -sf "$PWD"/bash-prompt-line.$HOME/.bash-prompt-line
+ln -sf "$PWD"/bash-prompt-line.bash $HOME/.bash-prompt-line
 echo "source $HOME/.bash-prompt-line" >> $HOME/.bashrc
